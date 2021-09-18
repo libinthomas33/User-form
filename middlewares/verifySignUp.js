ce9a0df606609120f7342063185e5f3815118e16
Libin <libinthomasntg@gmail.com>
2021-09-18 11:47:24 +0400

    api render fix

--- middlewares/verifySignUp.js
+++ middlewares/verifySignUp.js
@@ -5,15 +5,15 @@ function checkDuplicateEmail(req, res, next) {
     if (req.body.email) {
         userModel.findOne({ email: req.body.email }, (emailFetchErr, emailExists) => {
             if (emailFetchErr) {
-                return res.status(500).send(emailFetchErr);
+                return res.render('index', {title: 'User signup', messages: {error: 'Failed to check the email uniqueness.', success: ''}});
             }
             if (emailExists) {
-                return res.status(400).send({ message: 'Failed!. Email is already in use.' });
+                return res.render('index', {title: 'User signup', messages: {error: 'Failed!. Email is already in use.', success: ''}});
             }
             next();
         })
     } else {
-        return res.status(400).send({ message: 'Bad request. Parameters missing!!' });
+        return res.render('index', {title: 'User signup', messages: {error: 'Bad request. Parameters missing!!.', success: ''}})
     }
 }
 
