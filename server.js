5e201517cf542d0fcbf557cd0fb058227aef6608
Libin <libinthomasntg@gmail.com>
2021-09-18 11:54:45 +0400

    default page render fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //npm
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors =  require('cors');
+const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 
@@ -17,23 +17,23 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const PORT =  process.env.PORT || 8080;
-app.listen(PORT, ()=>{
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
     debugLog(`Server listening on ${PORT}`)
 })
 
 
 /* GET home page. */
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.set({
         'Access-control-Allow-Origin': '*'
-        });
-    return res.render('index', {title: 'User signup'});
+    });
+    return res.render('index', { title: 'User signup', messages: { error: '', success: '' } });
 })
 
 //navigate all api routes to index route
